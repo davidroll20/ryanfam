7cdb4874972d8cf77017b5211ca936a36552e7cf
David Herald <david@roll20.net>
2024-12-24 11:32:40 -0500

    Beginning to setup firebase auth with google signin

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,21 @@
-import { initializeApp } from 'firebase/app'
-import { collection, getFirestore } from 'firebase/firestore'
+import { initializeApp } from 'firebase/app';
+import { collection, getFirestore } from 'firebase/firestore';
 
 export const firebaseApp = initializeApp({
   apiKey: 'AIzaSyBQ6izD-t26QvAFhHV9SvmNy1cdt9fLaz8',
-  authDomain: 'ryan-fam.firebaseapp.com',
+  // authDomain: 'ryan-fam.firebaseapp.com',
+  authDomain: 'www.ryan-fam.com',
   projectId: 'ryan-fam',
   storageBucket: 'ryan-fam.firebasestorage.app',
   messagingSenderId: '750155303707',
   appId: '1:750155303707:web:d16ef825d3c2a14e93a1be',
-})
+});
 
-export const db = getFirestore(firebaseApp)
+export const db = getFirestore(firebaseApp);
 
 export enum ryanFamCollection {
   CALENDAR = 'calendar-events',
   BULLETIN = 'bulletins',
 }
-export const ryanFamCalendarRef = collection(db, ryanFamCollection.CALENDAR)
-export const ryanFamBulletinsRef = collection(db, ryanFamCollection.BULLETIN)
+export const ryanFamCalendarRef = collection(db, ryanFamCollection.CALENDAR);
+export const ryanFamBulletinsRef = collection(db, ryanFamCollection.BULLETIN);
