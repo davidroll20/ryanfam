b61b8d460573a795732c24fc37ede46e09a67af5
David Herald <david@roll20.net>
2024-12-24 17:13:37 -0500

    Added text to the About view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,33 @@
 <template>
   <main>
     <div class="about-view">
-      <h1>Email me at davidclaytonherald@gmail.com</h1>
+      <h1>Welcome to the Ryan Fam website!</h1>
+      <p>
+        If you're a family member, this is a place to help us keep up with each other. If you're
+        <i>not</i> a family member, I'm afraid there's not much here for you. If you're family, read
+        on!
+        <br />
+        <br />
+        Once you sign in, you'll be able to find upcoming events on the Calendar tab, or you can
+        head over to the Bulletin tab to see family news, recipes, and whatever else someone has
+        thought to contribute! If you're interested in posting something to the Bulletin yourself,
+        just press the big [+] button there to get started!
+        <br />
+        <br />
+        Right now, the site is pretty simple - we've got the aforementioned Calendar and Bulletin.
+        Yet to come are features like an Image Gallery and maybe a dedicated area for recipes, if
+        there's interest. Is there another feature you'd like to see added or tweaked? Let me know!
+        This site is made completely from scratch, so we are limited only by our creativity! If you
+        know who I am, you can just hit me up, but if you'd like to send any requests or feedback to
+        a dedicated place to help me out with organizing, you can email me at
+        <a href="mailto:contact@ryan-fam.com">contact@ryan-fam.com</a>. :)
+      </p>
     </div>
   </main>
 </template>
 
-<style></style>
+<style lang="scss">
+.about-view {
+  max-width: 900px;
+}
+</style>
